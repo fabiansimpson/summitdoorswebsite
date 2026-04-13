c4eb25450f7cc36d6ed9b1340ff7ffeb9237bca4
Fabian Simpson <125528034+fabiansimpson@users.noreply.github.com>
2026-04-13 13:36:12 +0200

    Workin on the what we do page
    added an image and redesigned the content heading. 
    made the side menu sticky, changed to content to 1 page with tabs. Put in placeholders. 
    Needs a lot more attention

--- js/products-body.js
+++ js/products-body.js
@@ -1,77 +1,24 @@
 "use strict";
-// const heroEl = document.querySelector(".js-hero-el");
-// const productsMenu = document.querySelector(".products-menu");
 
-// const makeMenuFixed = function (entries) {
-//   const [entrie] = entries;
-//   console.log(entrie);
-// if (entrie.isIntersecting) {
-//   productsMenu.classList.add("fixed-menu");
-// } else if (productsMenu.classList.contains("fixed-menu")) {
-//   // productsMenu.classList.add("bottom-menu");
-//   productsMenu.classList.remove("fixed-menu");
-// }
-// if (
-//   productsBody.isIntersecting === false &&
-//   )
-// ) {
-// } else if (productsBody.isIntersecting === true) {
-// }
-// };
+const tabs = document.querySelectorAll(".tabs-link");
+const tabsNavigation = document.querySelector(".tabs-nav");
+const tabsContent = document.querySelectorAll(".tab-content");
 
-// set up the revealsubheader. Is called upon from the forEach.
-// const productsBodyObserver = new IntersectionObserver(makeMenuFixed, {
-//   root: null,
-// rootMargin: "100% 0px 0px 0px",
-// threshold: 1,
-// root: document.querySelector(".products-menu"),
-// rootMargin: "-100px",
-// });
+tabsNavigation.addEventListener("click", function (e) {
+  const clicked = e.target.closest("a");
+  if (!clicked) return;
 
-// productsBodyObserver.observe(heroEl);
+  // Remove active classes
+  tabs.forEach((t) => {
+    t.classList.remove("tabs-link--active");
+  });
+  tabsContent.forEach((t) => {
+    t.classList.remove("tab-content--active");
+  });
 
-// console.log(productsBodyObserver);
-
-// console.log(
-//   "current sticky pos:",
-//   currStickyPos,
-//   "\n",
-//   "el top: ",
-//   productsBody.getBoundingClientRect().top,
-//   "\n pageyoffset:",
-//   window.pageYOffset,
-// );
-
-// console.log(productsBody.getBoundingClientRect().bottom, "\n");
-
-// console.log(productsBody.getBoundingClientRect().y);
-
-window.onscroll = function () {
-  const productsBody = document.querySelector(".products-body");
-  const stickyMenu = document.querySelector(".side-menu");
-  const sectionEl = document.querySelector("#architectural_services");
-  // const headerHeight = document.querySelector("header");
-  const productsBodyHeight =
-    productsBody.getBoundingClientRect().bottom -
-    (stickyMenu.getBoundingClientRect().height + 64);
-
-  const currStickyPos =
-    productsBody.getBoundingClientRect().top + window.pageYOffset;
-  console.log(currStickyPos);
-  if (
-    window.pageYOffset > currStickyPos &&
-    window.pageYOffset < productsBodyHeight
-  ) {
-    // stickyMenu.style.position("fi");
-    stickyMenu.style.top = stickyMenu.getBoundingClientRect().top;
-  } else {
-    stickyMenu.classList.remove("fixed-menu");
-  }
-};
-
-// const rect = productsBody.getBoundingClientRect();
-// console.log(rect);
-// window.addEventListener("scroll", function () {
-// console.log("bla");
-// console.log("Within scroll", rect);
-// });
+  // Add active classes
+  clicked.classList.add("tabs-link--active");
+  document
+    .querySelector(`.tab-content--${clicked.dataset.tab}`)
+    .classList.add("tab-content--active");
+});
