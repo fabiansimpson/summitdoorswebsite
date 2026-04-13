88eca72f63a6a1a6b9674e603382dbec173089f8
Fabian Simpson <125528034+fabiansimpson@users.noreply.github.com>
2026-04-14 00:02:35 +0200

    partial bugfixe
    tried to fixe a bug where javascript include script for html header doesn't get smoothscroll. It works most of the time but still suffers from the problem. 
    added the link in the header CTA button

--- js/script.js
+++ js/script.js
@@ -1,40 +1,5 @@
 "use strict";
 
-function includeHTML(includeComponent) {
-  var z, i, elmnt, file, wholeFile, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain attribute:*/
-    file = elmnt.getAttribute(includeComponent);
-    wholeFile = `/${file}`;
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-          if (this.status == 200) {
-            elmnt.innerHTML = this.responseText;
-          }
-          if (this.status == 404) {
-            elmnt.innerHTML = "Page not found.";
-          }
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute(includeComponent);
-          includeHTML();
-        }
-      };
-      xhttp.open("GET", wholeFile, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
-}
-includeHTML("include-header");
-includeHTML("include-footer");
-
 const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
